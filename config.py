9feff3f5271e392aa643d7b93ae99e07035ea0de
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-01-27 21:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,19 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    APP_ID = int(os.environ.get("23890262"))
+    API_HASH = os.environ.get("da7e86cf57b0e6220b8a9e0aed228a68
+
+API")
+    BOT_TOKEN = os.environ.get("5841568185:AAFODqetoPeeBhBsXDyRVT7KIniZDnpWOY8")
+    OWNER_ID = int(os.environ.get("1488316798"))
     # Chat bot's name
     CHAT_BOT_NAME = os.environ.get("CHAT_BOT_NAME", "Yui")
     # Your OpenAI API key
-    OPENAI_KEY = os.environ.get("OPENAI_KEY")
+    OPENAI_KEY = os.environ.get("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")
     # Your ARQ API Key
     ARQ_API_URL = "http://arq.hamker.dev"
-    ARQ_KEY = os.environ.get("ARQ_KEY")
+    ARQ_KEY = os.environ.get("NHVTTY-XEDFPD-XXCGHQ-UMQWZU-ARQ")
     # Default Chatbot engine you want to use after OpenAI
     DEFAULT_CHATBOT = os.environ.get("DEFAULT_CHATBOT", "affiliateplus")
     # Set ON_HEROKU to False if you aren't on heroku
